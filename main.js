99ffc31bab1bd9d782f32def5291b03f484040b0
Douglas-Green <varmevisible@gmail.com>
2024-04-08 18:02:48 -0400

    Pretty sure this is correct

--- main.js
+++ main.js
@@ -27,15 +27,19 @@ element.appendChild(p);
 const a = document.createElement("a");
 a.innerText = "I am a <a> tag";
 // BONUS: Add a link href to the <a>
-a.href = "https://www.google.com";
+a.href = "https://github.com/Douglas-Green/JS_DOM-Events";
 // TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
-
+element.appendChild(a);
 /*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
 
+let childNode = document.querySelector(".child-node");
+
 // TODO: Replace the "Child Node" with a new <p> element that reads "New Child Node"
-
+let newNode = document.createElement("p");
+newNode.textContent = "New Child Node";
+childNode.parentNode.replaceChild(newNode, childNode);
 // TODO: Remove the "New Child Node"
-
+newNode.parentNode.removeChild(newNode);
 /*----------- Exercise #4: LIST ITEMS ----------- */
 // Use the following array of values to generate a list on the DOM
 
@@ -53,16 +57,41 @@ let list = [
 ];
 
 // TODO: Create an unordered list element
-
+let ul = document.createElement("ul");
 // TODO: Iterate over the array values, and create a list item element for each
-
+list.forEach(item => {
+  let li = document.createElement("li");
+  li.textContent = item;
+  ul.appendChild(li);
+});
 // TODO: Append the new list items to the unordered list element
 
 // TODO: Append the unordered list to the `div#container` under exercise 4
-
+document.querySelector("#container").appendChild(ul);
 /*----------- Exercise #5: DOM EVENTS --------------*/
 
 // TODO: write a function called "show" which creates a new div with an alerting message to the user with this message
-// -> "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user"
-// This div should be a 'modal' that covers the main content on the screen
-// BONUS: The modal popup should be able to be closed. Refactor for this functionality
+function show() {
+  let modal = document.createElement("div");
+  modal.textContent =
+    "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user";
+  modal.style.position = "fixed";
+  modal.style.top = "0";
+  modal.style.left = "0";
+  modal.style.width = "100%";
+  modal.style.height = "100%";
+  modal.style.backgroundColor = "rgba(0,0,0,0.5)";
+  modal.style.color = "white";
+  modal.style.display = "flex";
+  modal.style.justifyContent = "center";
+  modal.style.alignItems = "center";
+  modal.style.zIndex = "1000";
+  // BONUS: The modal popup should be able to be closed. Refactor for this functionality
+
+  modal.addEventListener("click", () => {
+    document.body.removeChild(modal);
+  });
+  // -> "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user"
+  // This div should be a 'modal' that covers the main content on the screen
+  document.body.appendChild(modal);
+}
